70c1df4b64dca1cef3f277ea3f75859f45d7ff65
Destiny J. Wells <122392453+DJWtheCoder@users.noreply.github.com>
2023-09-03 16:36:03 -0400

    Third test case - refactor

--- test/test_grades.dart
+++ test/test_grades.dart
@@ -2,18 +2,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:wells_gradetool/gradeCalculator.dart';
 
 void main() {
+  gradeCalculator calculator = gradeCalculator();
   test('100 is an A', () {
-    gradeCalculator calculator = gradeCalculator();
     String expectLetterGrade = calculator.convertGrade(100);
     expect(expectLetterGrade, "A");
   });
   test('0 is an F', () {
-    gradeCalculator calculator = gradeCalculator();
     String expectLetterGrade = calculator.convertGrade(0);
     expect(expectLetterGrade, "F");
   });
   test('85 is an B', () {
-    gradeCalculator calculator = gradeCalculator();
     String expectLetterGrade = calculator.convertGrade(85);
     expect(expectLetterGrade, "B");
   });
