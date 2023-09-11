68ca0191c6a2baefb18fb81004cabda84b601f78
library <library@ul-edrln-6157.bsu.edu>
2023-09-11 17:32:28 -0400

    Refactor

--- lib/grade_calculator.dart
+++ lib/grade_calculator.dart
@@ -1,23 +1,17 @@
 class gradeCalculator {
   // This converts a grade percentage into a letter grade
   String convertGrade(int gradePercentage) {
-    if (_gradeIsBetween(gradePercentage, 101, 89)) {
+    if (gradePercentage >= 90) {
       return "A";
-    } else if (_gradeIsBetween(gradePercentage, 90, 79)) {
+    } else if (gradePercentage >= 80) {
       return "B";
-    }else if (_gradeIsBetween(gradePercentage, 80, 69)) {
+    }else if (gradePercentage >= 70) {
       return "C";
-    } else if (_gradeIsBetween(gradePercentage, 70, 59)) {
+    } else if (gradePercentage >= 60) {
       return "D";
-    }else if (_gradeIsBetween(gradePercentage, 60, -1)) {
+    }else {
       return "F";
-    } else {
-      return "Not valid grade";
     }
   }
 }
 
-// This checks if the grade percentage is less than the high grade and more than the low grade
-bool _gradeIsBetween(int gradePercentage, int highGrade, int lowGrade){
-  return gradePercentage > lowGrade && gradePercentage < highGrade;
-}
